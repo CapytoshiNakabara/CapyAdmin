8b7ec7c95fbf8bb28b31298f599de9c6e0a4244d
CapytoshiNakabara <164239947+CapytoshiNakabara@users.noreply.github.com>
2024-03-21 10:38:06 +0100

    Add amount to address

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import './App.css'
-// import { ethers } from 'ethers'
+import { ethers } from 'ethers'
 
 function App() {
   // const web3test = new Web3('https://go.getblock.io/cff88efce70347b2b8c66113eecefdd0'); // Use a BSC node
@@ -32,12 +32,7 @@ function App() {
     return allMinutes
   }
 
-  // const getBlockDate = (block) => {
-  //   const formattedTimestamp = ethers.formatUnits(block.timestamp, 0)
-  //   return new Date(formattedTimestamp * 1000)
-  // }
-
-  const logValues = async () => {
+  const logValues = async (isTest: boolean) => {
     setIsFetching(true)
 
     const fromDateAndTime = new Date(`${fromDate} ${fromHour}:${fromMinutes}`)
@@ -46,28 +41,41 @@ function App() {
     const fromSeconds = Math.floor(fromDateAndTime.getTime() / 1000)
     const toSeconds = Math.floor(toDateAndTime.getTime() / 1000)
 
-    const fromBlockResponse = await fetch(`https://api-testnet.bscscan.com/api?module=block&action=getblocknobytime&timestamp=${fromSeconds}&closest=before&apikey=7EYHFFCBKGK7TBDYXV8X4NH9Y8EBXA2CHX`)
+    const fromBlockResponse = await fetch(`https://api${isTest && "-testnet"}.bscscan.com/api?module=block&action=getblocknobytime&timestamp=${fromSeconds}&closest=before&apikey=7EYHFFCBKGK7TBDYXV8X4NH9Y8EBXA2CHX`)
     const fromBlock = await fromBlockResponse.json()
 
-    const toBlockResponse = await fetch(`https://api-testnet.bscscan.com/api?module=block&action=getblocknobytime&timestamp=${toSeconds}&closest=after&apikey=7EYHFFCBKGK7TBDYXV8X4NH9Y8EBXA2CHX`)
+    const toBlockResponse = await fetch(`https://api${isTest && "-testnet"}.bscscan.com/api?module=block&action=getblocknobytime&timestamp=${toSeconds}&closest=after&apikey=7EYHFFCBKGK7TBDYXV8X4NH9Y8EBXA2CHX`)
     const toBlock = await toBlockResponse.json()
 
-    const resullt = await fetch(`https://api-testnet.bscscan.com/api?module=account&action=tokentx&address=${fromAddress}&contractaddress=${tokenAddress}&startblock=${fromBlock.result}&endblock=${toBlock.result}&sort=asc&apikey=7EYHFFCBKGK7TBDYXV8X4NH9Y8EBXA2CHX`)
+    const resullt = await fetch(`https://api${isTest && "-testnet"}.bscscan.com/api?module=account&action=tokentx&address=${fromAddress}&contractaddress=${tokenAddress}&startblock=${fromBlock.result}&endblock=${toBlock.result}&sort=asc&apikey=7EYHFFCBKGK7TBDYXV8X4NH9Y8EBXA2CHX`)
     const allres = await resullt.json()
-    console.log(allres)
-    const distinctAddresses = new Set<string>(allres.result.map((res: any) => { return res.to }))
-    console.log(Array.from(distinctAddresses));
-    exportToCsv(Array.from(distinctAddresses))
+
+    let grouped = allres.result.reduce(
+      (result: any, currentValue: any) => {
+        (result[currentValue['to']] = result[currentValue['to']] || []).push(currentValue);
+        return result;
+      }, {});
+
+    let recieversWithAmount: any[] = []
+    Object.entries(grouped).forEach(([a, b]) => {
+      const values: [] = (b as any).map((ba: any) => ethers.formatUnits(ba.value, 18))
+      let amount = 0
+      values.forEach(val => amount += +val)
+      recieversWithAmount.push({ reviever: a, amount })
+    })
+    console.log(recieversWithAmount);
+    exportToCsv(recieversWithAmount)
 
     setIsFetching(false)
   }
 
-  const exportToCsv = (arrayToCsv: string[]) => {
+  const exportToCsv = (arrayToCsv: any[]) => {
 
-    let CsvString = "Address";
+    let CsvString = "Reciever,Amount,";
     CsvString += "\r\n";
     arrayToCsv.forEach((item, _index) => {
-      CsvString += item
+      CsvString += `${item.reviever},`
+      CsvString += `${item.amount},`
       CsvString += "\r\n";
     })
 
@@ -127,8 +135,13 @@ function App() {
         <input id="fromAddress" placeholder='From address' value={fromAddress} onChange={(e) => setFromAddress(e.target.value as any)} />
         <br />
 
-        <button onClick={() => logValues()}>
-          {isFetching ? "Loading..." : "Find buyers"}
+        <button onClick={() => logValues(false)}>
+          {isFetching ? "Loading..." : "Find buyers (Mainnet)"}
+        </button>
+        <br />
+
+        <button onClick={() => logValues(true)}>
+          {isFetching ? "Loading..." : "Find buyers (Testnet)"}
         </button>
       </div>
     </>
